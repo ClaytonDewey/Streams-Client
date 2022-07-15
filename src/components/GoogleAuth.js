55dd2319c5a958def62c6fa6fc15af933c0af0f1
ClaytonDewey <clay@dryadmedia.com>
2022-07-15 15:46:12 -0500

    Setup componentDidMount for auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "640034311295-4jatimd8e9a01ppqdi97upku514q8ui6.apps.googleusercontent.com",
+        scope: "email",
+        plugin_name: "streamy",
+      });
+    });
+  }
+
   render() {
     return <div>Google Auth</div>;
   }
