f59b450a644a950588b5516d3fdc18955ec202e0
ClaytonDewey <clay@dryadmedia.com>
2022-07-16 12:58:06 -0500

    Update StreamCreate

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderImput({ input }) {
+    return <input {...input} />;
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderImput} />
+        <Field name="description" component={this.renderImput} />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
