da1b7b31a8a948a1db6c6e161945375dbcc49833
ClaytonDewey <clay@dryadmedia.com>
2022-07-16 14:46:41 -0500

    Set up basic error handling

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,19 +2,50 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  renderImput({ input }) {
-    return <input {...input} />;
+  renderImput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
+
+  onSubmit(formValues) {
+    console.log(formValues);
   }
 
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderImput} />
-        <Field name="description" component={this.renderImput} />
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderImput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderImput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
 }
+
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
 
 export default reduxForm({
   form: "streamCreate",
